cc9753964453ef83ae3a7dee0aa73a224a9981d4
Mohamad Mahdi Ziaee <mahdi@swuser-129-8-216-95.swlan.csufresno.edu>
2016-09-19 11:53:22 -0700

    Completed reading the input file method.

--- MaxRangeSum/src/com/maxrangesum/main/Main.java
+++ MaxRangeSum/src/com/maxrangesum/main/Main.java
@@ -3,7 +3,7 @@ package com.maxrangesum.main;
 import java.io.*;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
 		File file = new File(args[0]);
 		@SuppressWarnings("resource")
@@ -12,7 +12,56 @@ public class Main {
 		while ((line = buffer.readLine()) != null) {
 			line = line.trim();
 			// Process line of input Here
+			Entry entry = readFile(line);
 		}
 	}
-	
+
+	public static Entry readFile(String line) {
+		int indexOfSemiColen = line.indexOf(";");
+		int numOfDays = Integer.valueOf(line.substring(0, indexOfSemiColen));
+		String[] strValues = line.substring(indexOfSemiColen + 1).split(" ");
+		int[] values = new int[strValues.length];
+		for (int index = 0; index < strValues.length; index++) {
+			values[index] = Integer.valueOf(strValues[index]);
+		}
+		return new Main().new Entry(numOfDays, values);
+	}
+
+	public class Entry {
+
+		private int numOfDays;
+		private int[] values;
+		private int result;
+
+		public Entry(int numOfDays, int[] values) {
+			this.numOfDays = numOfDays;
+			this.values = values;
+		}
+
+		public int getNumOfDays() {
+			return numOfDays;
+		}
+
+		public void setNumOfDays(int numOfDays) {
+			this.numOfDays = numOfDays;
+		}
+
+		public int[] getValues() {
+			return values;
+		}
+
+		public void setValues(int[] values) {
+			this.values = values;
+		}
+
+		public int getResult() {
+			return result;
+		}
+
+		public void setResult(int result) {
+			this.result = result;
+		}
+
+	}
+
 }
--- MaxRangeSum/src/com/maxrangesum/test/TestMain.java
+++ MaxRangeSum/src/com/maxrangesum/test/TestMain.java
@@ -4,11 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.maxrangesum.main.Main;
+import com.maxrangesum.main.Main.Entry;
+
 public class TestMain {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_readFile() {
+		String line = "5;7 -3 -10 4 2 8 -2 4 -5 -2";
+		Entry entry = Main.readFile(line);
+		assertEquals(5, entry.getNumOfDays());
+		assertEquals(10, entry.getValues().length);
+		assertEquals(7, entry.getValues()[0]);
+		assertEquals(-2, entry.getValues()[9]);
 	}
 
 }
