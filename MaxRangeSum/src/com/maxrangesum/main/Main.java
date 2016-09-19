cc89761e8a3109ef12ecad727e72468fd25f5120
Mohamad Mahdi Ziaee <mahdi@swuser-129-8-216-95.swlan.csufresno.edu>
2016-09-19 11:38:50 -0700

    Created a placeholder for Max Range Sum challenge.

---
+++ MaxRangeSum/src/com/maxrangesum/main/Main.java
@@ -0,0 +1,18 @@
+package com.maxrangesum.main;
+
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		File file = new File(args[0]);
+		@SuppressWarnings("resource")
+		BufferedReader buffer = new BufferedReader(new FileReader(file));
+		String line;
+		while ((line = buffer.readLine()) != null) {
+			line = line.trim();
+			// Process line of input Here
+		}
+	}
+	
+}
---
+++ MaxRangeSum/src/com/maxrangesum/test/TestMain.java
@@ -0,0 +1,14 @@
+package com.maxrangesum.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMain {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
